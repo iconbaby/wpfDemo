cc998656f31710947d4c51118caef5921bcf3f77
slkk <644187060@qq.com>
2018-12-12 16:39:28 +0800

    Exception window

--- HelloWpf/App.xaml.cs
+++ HelloWpf/App.xaml.cs
@@ -13,14 +13,20 @@ namespace HelloWpf
     /// </summary>
     public partial class App : Application
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
+        //private void Application_Startup(object sender, StartupEventArgs e)
+        //{
+        //    ExceptionWindow wnd = new ExceptionWindow();
+        //    wnd.Title = "ExceptionWindow ";
+        //    if (e.Args.Length == 1) {
+        //        //MessageBox.Show("Now opening file:\n\n" + e.Args[0]);
+        //    }
+        //    wnd.Show();
+        //}
+        //处理全局的异常
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            ResourceWindow wnd = new ResourceWindow();
-            wnd.Title = "Command window";
-            if (e.Args.Length == 1) {
-                //MessageBox.Show("Now opening file:\n\n" + e.Args[0]);
-            }
-            wnd.Show();
+            MessageBox.Show("Exception" + e.Exception.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
         }
     }
 }
---
+++ HelloWpf/ExceptionWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWpf
+{
+    /// <summary>
+    /// Interaction logic for ExceptionWindow.xaml
+    /// </summary>
+    public partial class ExceptionWindow : Window
+    {
+        public ExceptionWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            try
+            {
+                s.Trim();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("exception occure" + ex.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            s.Trim();
+        }
+    }
+}
