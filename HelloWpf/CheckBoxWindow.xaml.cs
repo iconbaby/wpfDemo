92f4e873bfa893a1b8860ac28d5f1c2f0355db9d
slkk <644187060@qq.com>
2018-12-12 23:00:26 +0800

    Checkbox Window

--- HelloWpf/CheckBoxWindow.xaml.cs
+++ HelloWpf/CheckBoxWindow.xaml.cs
@@ -23,5 +23,27 @@ namespace HelloWpf
         {
             InitializeComponent();
         }
+
+        private void CbAllFeature_Checked(object sender, RoutedEventArgs e)
+        {
+            bool newVal = cbAllFeature.IsChecked == true;
+            cbFeatureAbc.IsChecked = newVal;
+            cbFeatureXyz.IsChecked = newVal;
+            cbFeatureWww.IsChecked = newVal;
+        }
+
+
+        private void CbFeatureAbc_Checked(object sender, RoutedEventArgs e)
+        {
+            cbAllFeature.IsChecked = null;
+            if (cbFeatureAbc.IsChecked == true && cbFeatureXyz.IsChecked == true && cbFeatureWww.IsChecked == true) {
+                cbAllFeature.IsChecked = true;
+            }
+            if (cbFeatureAbc.IsChecked ==false && cbFeatureXyz.IsChecked == false && cbFeatureWww.IsChecked == false)
+            {
+                cbAllFeature.IsChecked = false;
+            }
+
+        }
     }
 }
