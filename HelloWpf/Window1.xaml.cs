feaddff7e56d53680b5610a2d59b515ec01742c0
slkk <644187060@qq.com>
2018-12-12 15:10:08 +0800

    event learning

---
+++ HelloWpf/Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWpf
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            pnlMainGrid.MouseDown += PnlMainGrid_MouseDown;
+            
+        }
+
+        private void PnlMainGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Your click Mouse Down at" + e.GetPosition(this).ToString());
+        }
+
+        private void PnlMainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
+        }
+
+
+    }
+}
