02810410a30c108d02773adcb2ab1c37383855b8
slkk <644187060@qq.com>
2018-12-14 10:43:12 +0800

    TabControl window

---
+++ HelloWpf/TabControlWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWpf
+{
+    /// <summary>
+    /// Interaction logic for TabControlWindow.xaml
+    /// </summary>
+    public partial class TabControlWindow : Window
+    {
+        public TabControlWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_selected_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Selected Item" + (tc_control.SelectedItem as TabItem).Content);
+        }
+
+        private void Btn_next_Click(object sender, RoutedEventArgs e)
+        {
+            int  newIndex =  tc_control.SelectedIndex + 1;
+            if (newIndex >= tc_control.Items.Count) {
+                newIndex = 0;
+            }
+            tc_control.SelectedIndex = newIndex;
+        }
+
+        private void Btn_prev_Click(object sender, RoutedEventArgs e)
+        {
+            int newIndex = tc_control.SelectedIndex - 1;
+            if (newIndex < 0) {
+                newIndex = tc_control.Items.Count - 1;
+            }
+            tc_control.SelectedIndex = newIndex;
+        }
+    }
+}
