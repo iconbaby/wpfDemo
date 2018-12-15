8de4987e29b7a9ada28ccb0f7b93ea8645c9e28c
slkk <644187060@qq.com>
2018-12-15 12:03:55 +0800

    listview sort

--- HelloWpf/ListViewWindow.xaml.cs
+++ HelloWpf/ListViewWindow.xaml.cs
@@ -11,7 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using System.Collections.Generic;
+using System.ComponentModel;
 namespace HelloWpf
 {
     /// <summary>
@@ -25,12 +26,17 @@ namespace HelloWpf
             List<UserList> listItem = new List<UserList>();
             listItem.Add(new UserList { Name = "jone", Age = 12, Email = "jone@163.com" ,Sex = SexType.Male});
             listItem.Add(new UserList { Name = "mike", Age = 24, Email = "mike@163.com",Sex=SexType.Male });
+            listItem.Add(new UserList { Name = "alien", Age = 87, Email = "alien@163.com", Sex=SexType.Male });
             listItem.Add(new UserList { Name = "jane", Age = 25, Email = "jane@163.com" ,Sex = SexType.Female});
             lv_connect.ItemsSource = listItem;
 
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lv_connect.ItemsSource);
             PropertyGroupDescription groupDescription = new PropertyGroupDescription("Sex");
+            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+            view.SortDescriptions.Add(new SortDescription("Age", ListSortDirection.Ascending));
             view.GroupDescriptions.Add(groupDescription);
+            
+            
         }
     }
     public enum SexType { Male,Female}
