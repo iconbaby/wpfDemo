688ff69a804800ddc5f11d093818be36b5227cd2
slkk <644187060@qq.com>
2018-12-15 12:39:08 +0800

    listview sort in header

--- HelloWpf/ListViewWindow.xaml.cs
+++ HelloWpf/ListViewWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace HelloWpf
     /// </summary>
     public partial class ListViewWindow : Window
     {
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
         public ListViewWindow()
         {
             InitializeComponent();
@@ -32,11 +34,70 @@ namespace HelloWpf
 
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lv_connect.ItemsSource);
             PropertyGroupDescription groupDescription = new PropertyGroupDescription("Sex");
-            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
-            view.SortDescriptions.Add(new SortDescription("Age", ListSortDirection.Ascending));
+            //view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+            //view.SortDescriptions.Add(new SortDescription("Age", ListSortDirection.Ascending));
             view.GroupDescriptions.Add(groupDescription);
-            
-            
+
+
+        }
+
+        private void lvUsersColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                lv_connect.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            lv_connect.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+    }
+
+    public class SortAdorner : Adorner
+    {
+        private static Geometry ascGeometry =
+            Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z");
+
+        private static Geometry descGeometry =
+            Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
+
+        public ListSortDirection Direction { get; private set; }
+
+        public SortAdorner(UIElement element, ListSortDirection dir)
+            : base(element)
+        {
+            this.Direction = dir;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            if (AdornedElement.RenderSize.Width < 20)
+                return;
+
+            TranslateTransform transform = new TranslateTransform
+                (
+                    AdornedElement.RenderSize.Width - 15,
+                    (AdornedElement.RenderSize.Height - 5) / 2
+                );
+            drawingContext.PushTransform(transform);
+
+            Geometry geometry = ascGeometry;
+            if (this.Direction == ListSortDirection.Descending)
+                geometry = descGeometry;
+            drawingContext.DrawGeometry(Brushes.Black, null, geometry);
+
+            drawingContext.Pop();
         }
     }
     public enum SexType { Male,Female}
